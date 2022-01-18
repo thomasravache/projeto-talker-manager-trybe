846385ff96f307bdf3e804fdc8227f6cefa9d558
thomasravache <thomasravache31@gmail.com>
2022-01-18 20:41:40 -0300

    Ajustando requisito 01

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const talker = require('./talker');
+const talkerRoute = require('./talker');
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,7 +13,7 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.use('/talker', talker);
+app.use('/talker', talkerRoute);
 
 app.listen(PORT, () => {
   console.log('Online');
--- talker.js
+++ talker.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const fs = require('fs').promises;
+const path = require('path');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const talkerFile = await fs.readFile('./talker.json', 'utf-8');
+  const talkerFile = await fs.readFile(path.join(__dirname, 'talker.json'), 'utf-8');
   const talkers = await JSON.parse(talkerFile);
 
   return res.status(200).json(talkers);
