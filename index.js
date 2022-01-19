2c2c01cfafdc9a42df343211e3d8a6edabd0d3ed
thomasravache <thomasravache31@gmail.com>
2022-01-19 01:33:34 -0300

    Requisito 03

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const talkerRoute = require('./talker');
+const loginRoute = require('./login');
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,6 +15,7 @@ app.get('/', (_request, response) => {
 });
 
 app.use('/talker', talkerRoute);
+app.use('/login', loginRoute);
 
 app.listen(PORT, () => {
   console.log('Online');
---
+++ login.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const crypto = require('crypto');
+
+const router = express.Router();
+
+const generateToken = () => crypto.randomBytes(8).toString('hex');
+
+router.post('/', (req, res) => {
+  const { email, password } = req.body;
+  const emailValidator = /\S+@\S+\.\S+/;
+  const isValidEmail = emailValidator.test(email);
+
+  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  if (!isValidEmail) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  
+  const token = generateToken();
+
+  return res.status(200).json({ token });
+});
+
+module.exports = router;
